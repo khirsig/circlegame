16f2597dbcdf882b0dab727378b1369024fb9201
Kevin Hirsig <khirsig@1-B-4.42heilbronn.de>
2022-04-28 09:06:22 +0200

    small adjustments to config file

--- config.hpp
+++ config.hpp
@@ -6,7 +6,7 @@
 /*   By: khirsig <khirsig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 10:00:34 by khirsig           #+#    #+#             */
-/*   Updated: 2022/04/27 18:18:23 by khirsig          ###   ########.fr       */
+/*   Updated: 2022/04/28 09:05:29 by khirsig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 # define CONFIG_HPP
 
 //	Define screen ratio. Has to be 16x9. As example: 1920x1080
-# define SCREEN_WIDTH 1408
-# define SCREEN_HEIGHT 792
+# define SCREEN_WIDTH 1920
+# define SCREEN_HEIGHT 1080
 
 //	Define screen FPS
 # define SCREEN_FPS 123
-
-//	Define player count
-# define PLAYER_COUNT 1
 
 extern int		screenWidth;
 extern int		screenHeight;
